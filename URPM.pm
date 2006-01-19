30a0606a82525062a9647d33f52adab66a6ceaac
rgarciasuarez <rgarciasuarez@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2006-01-19 13:31:22 +0000

    Rewrite and relocate doc at a more proper place. Update copyright notice.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/rpm/perl-URPM/trunk@11413 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- URPM.pm
+++ URPM.pm
@@ -346,6 +346,13 @@ Recognized options are:
 
 =back
 
+=item URPM::spec2srcheader($specfile)
+
+Returns a URPM::Package object containing the header of the source rpm produced
+by the evaluation of the specfile whose path is given as argument. All
+dependencies stored in this header are exactly the one needed to build the
+specfile.
+
 =head2 The URPM::DB class
 
 =over 4
@@ -671,7 +678,7 @@ Expands the specified macro.
 
 Define a macro. For example,
 
-    URPM::add_macro("vendor Mandrakesoft");
+    URPM::add_macro("vendor Mandriva");
     my $vendor = URPM::expand("%vendor");
 
 The 'noexpand' version doesn't expand literal newline characters in the
@@ -694,19 +701,13 @@ Sets rpm verbosity level. $level is an integer between 2 (RPMMESS_CRIT) and 7
 
 =item rpmErrorWriteTo($fd)
 
-=item spec2srcheader($specfile)
-
-Return a URPM::Package containing the header of source rpm produce by the
-evaluation of $specfile. Which mean all dependencies are exactally the one
-need to build the specfile.
-
 =back
 
 =head1 COPYRIGHT
 
 Copyright 2002, 2003, 2004, 2005 MandrakeSoft SA
 
-Copyright 2005 Mandriva SA
+Copyright 2005, 2006 Mandriva SA
 
 Original author: FranE<ccedil>ois Pons.
 Current maintainer: Rafael Garcia-Suarez
