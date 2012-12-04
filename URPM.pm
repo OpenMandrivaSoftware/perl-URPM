d69d0ab66aed9fb50a7f2dc1c9febca0f06a68fd
tv@5b7c3c08-40e5-403b-9995-ace06908e4af <tv@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-12-04 16:28:51 +0000

    (packages_providing) sort package ids before returning them
    this mean prefering package from first medium when same NEVR exists in
    several media, thus prefering 64bit media over 32 bit ones (mga#7524)
    
    complement commit r2909 on Feb 7 2012:
    " (find_required_package) really prefer packages from first media, thus
    fixing downloading noarch packages from 32 bit repository (mga#1603)"
    
    git-svn-id: svn://svn.mageia.org/soft/rpm/perl-URPM/trunk@6567 5b7c3c08-40e5-403b-9995-ace06908e4af

--- URPM.pm
+++ URPM.pm
@@ -30,7 +30,7 @@ sub set_nofatal { $_[0]{nofatal} = $_[1] }
 
 sub packages_providing {
     my ($urpm, $name) = @_;
-    grep { $_ } map { $urpm->{depslist}[$_] } keys %{$urpm->{provides}{$name} || {}};
+    grep { $_ } map { $urpm->{depslist}[$_] } sort { $a <=> $b } keys %{$urpm->{provides}{$name} || {}};
 }
 
 sub packages_obsoleting {
