e36cd70553e61a0dcdee6a40fce1839ddb99861f
tv@5b7c3c08-40e5-403b-9995-ace06908e4af <tv@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-12-04 16:28:54 +0000

    (find_required_package) previous commit enables us to not reinvent packages_providing()
    git-svn-id: svn://svn.mageia.org/soft/rpm/perl-URPM/trunk@6568 5b7c3c08-40e5-403b-9995-ace06908e4af

--- URPM/Resolve.pm
+++ URPM/Resolve.pm
@@ -219,12 +219,11 @@ sub find_required_package {
 	    $may_add_to_packages->($pkg);
 	} elsif (my $name = property2name($_)) {
 	    my $property = $_;
-	    foreach (keys %{$urpm->{provides}{$name} || {}}) {
-		my $pkg = $urpm->{depslist}[$_];
+	    foreach my $pkg (packages_providing($urpm, $name)) {
 		$pkg->is_arch_compat or next;
 		$pkg->flag_skip || $state->{rejected}{$pkg->fullname} and next;
 		#- check if at least one provide of the package overlaps the property
-		if (!$urpm->{provides}{$name}{$_} || $pkg->provides_overlap($property)) {
+		if (!$urpm->{provides}{$name}{$pkg->id} || $pkg->provides_overlap($property)) {
 		    #- determine if this package is better than a possibly previously chosen package.
 		    $pkg->flag_selected || exists $state->{selected}{$pkg->id} and return [$pkg];
 		    !$strict_arch || strict_arch_check_installed($db, $pkg) or next;
