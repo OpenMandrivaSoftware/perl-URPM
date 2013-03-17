OpenMandrivaSoftware/perl-URPM

commit b253bdde96a1d97de95ca464cd01023c4321a41f

(_run_cb_while_traversing) introduce this helper
git-svn-id: svn://svn.mageia.org/soft/rpm/perl-URPM/trunk@4947 5b7c3c08-40e5-403b-9995-ace06908e4af

URPM.xs
@@ -159,6 +159,24 @@ rpmError_callback() {
   return RPMLOG_DEFAULT;
 }
 
+static inline int _run_cb_while_traversing(SV *callback, Header header) {
+     dSP;
+     URPM__Package pkg = calloc(1, sizeof(struct s_Package));
+
+     pkg->flag = FLAG_ID_INVALID | FLAG_NO_HEADER_FREE;
+     pkg->h = header;
+
+     PUSHMARK(SP);
+     mXPUSHs(sv_setref_pv(newSVpvs(""), "URPM::Package", pkg));
+     PUTBACK;
+
+     int count = call_sv(callback, G_SCALAR);
+
+     SPAGAIN;
+     pkg->h = NULL; /* avoid using it anymore, in case it has been copied inside callback */
+     return count;
+}
+
 static inline  void _header_free(URPM__Package pkg) {
   if (pkg->h && !(pkg->flag & FLAG_NO_HEADER_FREE))
     pkg->h = headerFree(pkg->h);
@@ -2916,20 +2934,7 @@ Db_traverse(db,callback)
   mi = rpmtsInitIterator(db->ts, RPMDBI_PACKAGES, NULL, 0);
   while ((header = rpmmiNext(mi))) {
     if (SvROK(callback)) {
-      dSP;
-      URPM__Package pkg = calloc(1, sizeof(struct s_Package));
-
-      pkg->flag = FLAG_ID_INVALID | FLAG_NO_HEADER_FREE;
-      pkg->h = header;
-
-      PUSHMARK(SP);
-      mXPUSHs(sv_setref_pv(newSVpvs(""), "URPM::Package", pkg));
-      PUTBACK;
-
-      call_sv(callback, G_DISCARD | G_SCALAR);
-
-      SPAGAIN;
-      pkg->h = NULL; /* avoid using it anymore, in case it has been copied inside callback */
+         _run_cb_while_traversing(callback, header);
     }
     ++count;
   }
@@ -2967,20 +2972,7 @@ Db_traverse_tag(db,tag,names,callback)
       mi = rpmtsInitIterator(db->ts, rpmtag, name, str_len);
       while ((header = rpmmiNext(mi))) {
 	if (SvROK(callback)) {
-	  dSP;
-	  URPM__Package pkg = calloc(1, sizeof(struct s_Package));
-
-	  pkg->flag = FLAG_ID_INVALID | FLAG_NO_HEADER_FREE;
-	  pkg->h = header;
-
-	  PUSHMARK(SP);
-	  mXPUSHs(sv_setref_pv(newSVpvs(""), "URPM::Package", pkg));
-	  PUTBACK;
-
-	  call_sv(callback, G_DISCARD | G_SCALAR);
-
-	  SPAGAIN;
-	  pkg->h = NULL; /* avoid using it anymore, in case it has been copied inside callback */
+	  _run_cb_while_traversing(callback, header);
 	}
 	++count;
       }
@@ -3009,20 +3001,7 @@ Db_traverse_tag_find(db,tag,name,callback)
   ts_nosignature(db->ts);
   mi = rpmtsInitIterator(db->ts, rpmtag, name, 0);
   while ((header = rpmmiNext(mi))) {
-      dSP;
-      URPM__Package pkg = calloc(1, sizeof(struct s_Package));
-
-      pkg->flag = FLAG_ID_INVALID | FLAG_NO_HEADER_FREE;
-      pkg->h = header;
-
-      PUSHMARK(SP);
-      mXPUSHs(sv_setref_pv(newSVpvs(""), "URPM::Package", pkg));
-      PUTBACK;
-
-      int count = call_sv(callback, G_SCALAR);
-
-      SPAGAIN;
-      pkg->h = NULL; /* avoid using it anymore, in case it has been copied inside callback */
+      int count = _run_cb_while_traversing(callback, header);
 
       if (count == 1 && POPi) {
 	found = 1;

commit 42ca309ebe499cd74f051fb8874cc2299cbd0aeb

(Db_traverse_tag_find) fix a rare segfault
found accidently

could be triggered by the following (calling enough times either
requires() or provides() while traversing rpm db) which resulting in
an "Invalid read of size 8":

use URPM;
my $db = URPM::DB::open()->traverse_tag_find('whatprovides', 'kernel',
sub {
    my ($p) = @_;
    warn $p->requires, $p->requires, $p->requires, $p->requires, $p->requires,
    $p->requires, $p->requires, $p->requires, $p->requires, $p->requires, $p->requires,
    $p->requires, $p->requires, $p->requires, $p->requires, $p->requires, $p->requires;
    return 1;
});

bug introduced in commit r4947 on June 14 2012:
"(_run_cb_while_traversing) introduce this helper"

git-svn-id: svn://svn.mageia.org/soft/rpm/perl-URPM/trunk@6551 5b7c3c08-40e5-403b-9995-ace06908e4af

URPM.xs
@@ -2996,8 +2996,10 @@ Db_traverse_tag_find(db,tag,name,callback)
   ts_nosignature(db->ts);
   mi = rpmtsInitIterator(db->ts, rpmtag, name, 0);
   while ((header = rpmmiNext(mi))) {
+      dSP,
       int count = _run_cb_while_traversing(callback, header, 0);
 
+      SPAGAIN;
       if (count == 1 && POPi) {
 	found = 1;
 	break;